87b3f70fc9d2c7609f5f4f65520644cf4806b5d6
Roman <149431633+Rokiart@users.noreply.github.com>
2023-12-27 16:35:40 +0100

    Create V02ZimskoVjezbanje.cs

---
+++ CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("V02")]
+    public class V02ZimskoVjezbanje : ControllerBase
+    {
+        [HttpGet]
+        [Route("zad1")]
+        public int[] Zad1(int x)
+         //bilo koji unos ispisuje brojeve od 0 do 100!  
+        {
+            x = 100;
+            int[] niz = new int[x];
+            for(int i=0;i<x;i++)
+            {
+
+               niz[i]= i+1;
+            }  return niz;
+           
+          
+
+        }
+
+        [HttpGet]
+        [Route("zad2")]
+
+
+
+
+
+    }
+
+
+}
