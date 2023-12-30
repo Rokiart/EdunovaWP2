f4f13189a68eb7b7aebc13e125615885934c2c4e
Roman <149431633+Rokiart@users.noreply.github.com>
2023-12-30 19:45:07 +0100

    Update V02ZimskoVjezbanje.cs

--- CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -11,7 +11,7 @@ namespace WebAPI.Controllers
         [HttpGet]
         [Route("zad1")]
         public int[] Zad1(int x)
-         //bilo koji unos ispisuje brojeve od 0 do 100!  
+         //ruta prima jedan parametar te vraca brojeve od 0 do 100!  
         {
             x = 100;
             int[] niz = new int[x];
@@ -24,7 +24,8 @@ namespace WebAPI.Controllers
           
 
         }
-//*************************************************************************
+        //*************************************************************************
+        //Ruta ne prima niti jedan parametar i vraća zbroj prvih 100 brojeva
         [HttpGet]
         [Route("zad2")]
         public int Zad2()
@@ -39,34 +40,81 @@ namespace WebAPI.Controllers
             return x;
         }
         //**************************************************************************
+        // Ruta ne prima niti jedan parametar i vraća niz s svim parnim brojevima od 1 do 57
         [HttpPost]
         [Route("zad3")]
         public int[] Zad3()
         {
-            int[] x = new int[(57 % 2)];
+            int[] x = new int[(58 / 2)];
             int unosx = 0;
             for (int i = 0; i <= 57; i += 2)
             {
-                x[unosx] = i;
+                x [unosx] = i;
                 unosx++;
             }
             return x;
         }
         //**************************************************************************
-        //[HttpPost]
-        //[Route("Zad4")]
+        //Ruta ne prima niti jedan parametar i vraća zbroj svih parnih brojeva od 2 do 18
+        [HttpPost]
+        [Route("zad4")]
+        public int Zad4()
 
-        //public int[] Zad4()
-        //{
-        //    int[] NizParnihBrojeva = new int[(58 / 2)];
-        //    int index = 0;
-        //    for (int i = 0; i <= 57; i += 2)
-        //    {
-        //        NizParnihBrojeva[index] = i;
-        //        index++;
-        //    }
-        //    return NizParnihBrojeva;
-        //}
+        {
+            int x = 0;
+
+            for (int i = 0; i <= 18; i += 2)
+            {
+                x += i;
+            }
+            return x;
+        }
+        //**************************************************************************
+        //Ruta prima jedan parametar koji je cijli broj i vraća zbroj svih brojava od 1 do primljenog broja
+        [HttpGet]
+        [Route("zad5")]
+        public int Zad5 (int zbroj)
+        {
+            int suma = 0;
+            for (int i = 1; i <= zbroj; i++)
+            {
+                suma += i;
+            }
+            return suma;
+        }
+        //***************************************************************************
+        // Ruta prima dva parametra koji su cijeli brojevi i vraća niz s svim parnim brojevima između primljenih brojeva
+        [HttpGet]
+        [Route("zad6")]
+        public int Zad6(int pb,int db)
+            // pb-prvi broj , db-drugi broj
+        {
+            int x = 0;
+
+            for (int i = pb; i <= db; i += 2)
+            {
+                x += i;
+            }
+            return x;
+        }
+        //****************************************************************************
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća niz s svim neparnim brojevima između primljenih brojeva
+        [HttpPost]
+        [Route("zad7")]
+        public int[] Zad7(int pb,int db)
+        {
+            int[] x = new int[(db / pb)];
+            int unosx = 0;
+            for (int i = pb; i <= db; i += 2)
+            {
+                x[unosx] = i+1;
+                unosx++;
+            }
+            return x;
+        }
+        //****************************************************************************
+
+
 
     }
 
